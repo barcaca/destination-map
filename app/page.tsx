d5b4b9ae19f5723f13e8b21fb6900b235d94932e
barcaca <73846225+barcaca@users.noreply.github.com>
2024-12-13 16:04:09 -0300

    feat: add page to view places by continent

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,14 @@
-import { HeroCarrousel } from '@/components/hero-carrousel'
 import { Recommendations } from '@/components/recommendations'
 import { SectionContinents } from '@/components/section-continents'
 
 export default function Home() {
   return (
     <>
-      <HeroCarrousel />
       <div className="bg-background text-foreground">
-        <div className="space-y-16 py-16">
+        <main className="space-y-16 py-16">
           <Recommendations />
           <SectionContinents />
-        </div>
+        </main>
       </div>
     </>
   )
