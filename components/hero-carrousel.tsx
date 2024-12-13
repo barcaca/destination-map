0b6342b03d86e9565c9615915749f12ebb010772
barcaca <73846225+barcaca@users.noreply.github.com>
2024-12-13 14:35:13 -0300

    style: update styles in HeroCarousel

--- components/hero-carrousel.tsx
+++ components/hero-carrousel.tsx
@@ -39,20 +39,22 @@ export function HeroCarrousel() {
         <CarouselContent>
           {slides.map(slide => (
             <CarouselItem key={slide.id}>
-              <div className="relative h-[80vh] w-full">
-                <Image
-                  src={slide.image}
-                  alt={slide.alt}
-                  className="object-cover"
-                  priority
-                  fill
-                  aria-hidden="true"
-                />
-                <div className="absolute inset-0 flex items-center justify-center bg-black/50">
-                  <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-                    <h1 className="text-center font-bold text-4xl text-white md:text-6xl">
-                      {slide.title}
-                    </h1>
+              <div className="relative overflow-hidden rounded-lg p-1">
+                <div className="relative h-[80vh] w-full overflow-hidden rounded-xl">
+                  <Image
+                    src={slide.image}
+                    alt={slide.alt}
+                    className="object-cover"
+                    priority
+                    fill
+                    aria-hidden="true"
+                  />
+                  <div className="absolute inset-0 flex items-center justify-center bg-black/50">
+                    <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+                      <h1 className="text-center font-bold text-4xl text-white md:text-6xl">
+                        {slide.title}
+                      </h1>
+                    </div>
                   </div>
                 </div>
               </div>
