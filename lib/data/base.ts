1d50eda0d8fb8a18f2a2cf5c4d85ae06029d89c7
barcaca <73846225+barcaca@users.noreply.github.com>
2024-12-18 17:16:57 -0300

    chore: add base.ts with BASE_URL and ENDPOINTS

---
+++ lib/data/base.ts
@@ -0,0 +1,9 @@
+export const BASE_URL = {
+  REST: 'https://destination-map-backend.vercel.app/api',
+} as const
+
+export const ENDPOINTS = {
+  PLACES: '/places',
+  USERS: '/users',
+  FAVORITE: '/favorite',
+} as const
