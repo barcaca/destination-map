f1b6b91d0c26676662c4c17e62620de2cf661821
barcaca <73846225+barcaca@users.noreply.github.com>
2024-12-18 17:08:54 -0300

    chore: update types for Places

--- types/place.ts
+++ types/place.ts
@@ -1,7 +1,28 @@
+export type Favorites = {
+  id: number
+  user_id: string
+  place_id: number[]
+}
+
+export type User = {
+  id: string
+  email: string
+  favorites_id: number
+}
+
 export type Location = {
-  continent: string
-  country: string
-  state: string
+  continent: {
+    id: number
+    name: string
+  }
+  country: {
+    id: number
+    name: string
+  }
+  state: {
+    id: number
+    name: string
+  }
 }
 
 export type Place = {
@@ -10,5 +31,31 @@ export type Place = {
   description: string
   location: Location
   images: string[]
+  user_id: string
   favorite: boolean
+}
+
+export type FavoritePlaces = {
+  placesId: string[]
+}
+
+export type Region = {
+  id: number
+  name: string
+}
+
+export type Country = {
+  id: number
+  name: string
+  region: string
+  region_id: number
+  translations: {
+    'pt-BR': string
+  }
+}
+
+export type State = {
+  id: number
+  name: string
+  country_id: number
 }
