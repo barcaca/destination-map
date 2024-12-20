488f555d805e7b871e5398943aac732efb659f1c
barcaca <73846225+barcaca@users.noreply.github.com>
2024-12-20 15:21:10 -0300

    fix: improve CustomToast functionality

--- lib/custom-toast.ts
+++ lib/custom-toast.ts
@@ -19,59 +19,54 @@ export type ToastState = {
  * customToast({ status: 404, message: 'Page not found' });
  * ```
  */
-export function customToast(data: ToastState) {
-  setTimeout(() => {
-    switch (data.status) {
-      case 200:
-        toast.success(data.message || 'Operação bem-sucedida.')
-        break
-      case 201:
-        toast.success(data.message || 'Recurso criado com sucesso.')
-        break
-      case 204:
-        toast.info(data.message || 'Nenhum conteúdo para mostrar.')
-        break
-      case 400:
-        toast.error(
-          data.message || 'Solicitação inválida. Verifique os dados fornecidos.'
-        )
-        break
-      case 401:
-        toast.error(
-          data.message || 'Não autorizado. Verifique suas credenciais.'
-        )
-        break
-      case 403:
-        toast.error(
-          data.message ||
-            'Proibido. Você não tem permissão para realizar esta ação.'
-        )
-        break
-      case 404:
-        toast.error(data.message || 'Recurso não encontrado.')
-        break
-      case 409:
-        toast.error(data.message || 'Conflito. Verifique os dados fornecidos.')
-        break
-      case 429:
-        toast.warning(
-          data.message || 'Muitas solicitações. Tente novamente mais tarde.'
-        )
-        break
-      case 500:
-        toast.error(
-          data.message ||
-            'Erro interno no servidor. Tente novamente mais tarde.'
-        )
-        break
-      case 503:
-        toast.warning(
-          data.message || 'Serviço indisponível. Tente novamente mais tarde.'
-        )
-        break
-      default:
-        toast.error(data.message || 'Erro desconhecido.')
-        break
-    }
-  }, 100)
+export function customToast(data: ToastState): void {
+  switch (data.status) {
+    case 200:
+      toast.success(data.message || 'Operação bem-sucedida.')
+      break
+    case 201:
+      toast.success(data.message || 'Recurso criado com sucesso.')
+      break
+    case 204:
+      toast.info(data.message || 'Nenhum conteúdo para mostrar.')
+      break
+    case 400:
+      toast.error(
+        data.message || 'Solicitação inválida. Verifique os dados fornecidos.'
+      )
+      break
+    case 401:
+      toast.error(data.message || 'Não autorizado. Verifique suas credenciais.')
+      break
+    case 403:
+      toast.error(
+        data.message ||
+          'Proibido. Você não tem permissão para realizar esta ação.'
+      )
+      break
+    case 404:
+      toast.error(data.message || 'Recurso não encontrado.')
+      break
+    case 409:
+      toast.error(data.message || 'Conflito. Verifique os dados fornecidos.')
+      break
+    case 429:
+      toast.warning(
+        data.message || 'Muitas solicitações. Tente novamente mais tarde.'
+      )
+      break
+    case 500:
+      toast.error(
+        data.message || 'Erro interno no servidor. Tente novamente mais tarde.'
+      )
+      break
+    case 503:
+      toast.warning(
+        data.message || 'Serviço indisponível. Tente novamente mais tarde.'
+      )
+      break
+    default:
+      toast.error(data.message || 'Erro desconhecido.')
+      break
+  }
 }
